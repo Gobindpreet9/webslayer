bba42ebd6ed4eb6b3c0b1bed74e6e0aa91eb7e67
Gobindpreet Makkar <54003957+Gobindpreet9@users.noreply.github.com>
2024-08-17 12:56:36 -0700

    format output response

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from schema.event_schema import EventsSchema
 from scraper.scraper import Scraper
 import logging
 from utils.config import Config
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -9,7 +10,8 @@ logger = logging.getLogger(__name__)
 def main(urls, tags, schema):
     extractor = Scraper(schema, urls, logger, crawl_website=True, crawl_max_depth=2)
     extracted_data = extractor.extract()
-    logger.info(f"Response:\n{extracted_data['generation']}")  # todo format the json to make it more readable
+    formatted_response = json.dumps(extracted_data['generation'], indent=4)
+    logger.info(f"Response:\n{formatted_response}")
 
 
 if __name__ == "__main__":
